2456cc1b659a830d90f4e7a3c56551c9c19791e1
vunguyenhuong <lmht1717@gmail.com>
2025-08-20 17:42:29 +0700

    update

--- pages/api/auth.js
+++ pages/api/auth.js
@@ -1,6 +1,8 @@
-export default function handler(req, res) {
+// pages/api/auth.js
+export default async function handler(req, res) {
   if (req.method === "POST") {
-    // Aruba chỉ cần HTTP 200 + chữ "Authenticated"
+    // Aruba chỉ quan tâm response này
+    res.setHeader("Content-Type", "text/plain");
     res.status(200).send("Authenticated");
   } else {
     res.status(405).send("Method Not Allowed");
